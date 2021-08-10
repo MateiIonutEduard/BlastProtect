ac46226f033470c89b2cfd95476d4d9dc6d0e7e8
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2021-08-10 12:48:21 +0300

    Update Features
    Update project files.

--- Assets/Scripts/LoadMap.cs
+++ Assets/Scripts/LoadMap.cs
@@ -52,6 +52,7 @@ public class LoadMap : MonoBehaviour
             {
                 player.GetComponent<PlayerController>().enabled = false;
                 player.GetComponent<EnemyController>().enabled = true;
+                player.GetComponent<EnemyController>().mode = (PlayerType)Random.Range(0, 5);
             }
             else
             {
