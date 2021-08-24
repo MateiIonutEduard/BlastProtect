eec376d295fcf14c3eace68f27ddd8c540471ac3
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2021-08-24 10:34:25 +0300

    Add Features
    Add new features.

---
+++ Assets/Scripts/UI/Gameplay/Window.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Window : MonoBehaviour
+{
+    public GameObject OptionsWindow;
+    private bool IsActive;
+
+    public void Awake()
+    {
+        IsActive = false;
+    }
+
+    public void OnClose()
+    {
+        IsActive = false;
+        OptionsWindow.SetActive(IsActive);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            IsActive = !IsActive;
+            OptionsWindow.SetActive(IsActive);
+        }
+    }
+}
